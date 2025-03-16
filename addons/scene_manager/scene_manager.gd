5f5f5716a9a434dbe885e0a530f944a77b7f8701
amey-bhandari <ameyb@purdue.edu>
2025-03-16 11:54:03 -0400

    fixed?

--- addons/scene_manager/scene_manager.gd
+++ addons/scene_manager/scene_manager.gd
@@ -50,14 +50,11 @@ func switch_scene(target: GameState):
 	SceneManager.no_effect_change_scene(get_scene_string(target))
 
 
-const max_round: int = 3
 #const max_round: int = 3
 const max_round: int = 1
 @onready var game_state: GameState = GameState.STATE_MENU
 @onready var round_number: int = 0
 @onready var round_score: int = 0
-@onready var stars_collected: int = 0
-@onready var typing_content: String = ""
 @onready var minigame_stars_collected: int = 0
 @onready var total_stars_collected: int = 0
 @onready var typing_content: String = "eeeee"
