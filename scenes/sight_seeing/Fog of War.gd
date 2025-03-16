6e422f4df1044712849bf1418fbcd5b52859c7b2
amey-bhandari <ameyb@purdue.edu>
2025-03-16 11:48:40 -0400

    minigame-1 finished

--- scenes/sight_seeing/Fog of War.gd
+++ scenes/sight_seeing/Fog of War.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+var speed_multiplier = 0.75
+
 func _physics_process(delta: float) -> void:
 	var vel: Vector2 = get_global_mouse_position() - global_position
-	velocity = vel
+	velocity = vel * speed_multiplier
 	move_and_slide()
